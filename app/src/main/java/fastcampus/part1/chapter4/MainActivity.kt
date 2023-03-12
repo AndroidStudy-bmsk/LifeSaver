503ec71e333db18ed3a4047c0a0f0df3425fb590
bmsk <vv99911@gmail.com>
2023-03-12 22:41:47 +0900

    [Feat] activity_main 구성
    ### resource
    - styles.xml 생성
      - 중복되는 코드들 통합
    ### layout
    - baseline
    - style 적용
    - maxLines 적용
    - ellipsize 적용
    - Guideline 적용

--- app/src/main/java/fastcampus/part1/chapter4/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/chapter4/MainActivity.kt
@@ -2,7 +2,6 @@ package fastcampus.part1.chapter4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import fastcampus.part1.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
