072cbbc29410a6abb7befb006a733e76380c1bcb
bmsk <vv99911@gmail.com>
2023-03-12 23:56:41 +0900

    [Add] Spinner & arrays.xml
    - Spinner 어댑터 추가
    - arrays.xml 추가

--- app/src/main/java/fastcampus/part1/chapter4/EditActivity.kt
+++ app/src/main/java/fastcampus/part1/chapter4/EditActivity.kt
@@ -2,14 +2,28 @@ package fastcampus.part1.chapter4
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import fastcampus.part1.chapter4.databinding.ActivityEditBinding
 
-class EditActivity : AppCompatActivity(){
+class EditActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
-        val message = intent.getStringExtra("intentMessage") ?: "No Message"
-        Log.d("IntentMessage", message)
+        initBinding()
+
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.blood_types,
+            android.R.layout.simple_list_item_1
+        )
+    }
+
+    private fun initBinding() {
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
