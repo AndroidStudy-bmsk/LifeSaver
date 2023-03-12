7fb6e2c44b14a07306e8be5441fe70af8fc0ed48
bmsk <vv99911@gmail.com>
2023-03-12 23:10:36 +0900

    [Add] InputActivity, floatingactionbutton
    - floatingactionbutton 추가
    - intent 추가 (to InputActivity)
    
    ### resource
    - baseline_edit 추가 (floatingactionbutton)
    - style에 gravity 적용

---
+++ app/src/main/java/fastcampus/part1/chapter4/InputActivity.kt
@@ -0,0 +1,13 @@
+package fastcampus.part1.chapter4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InputActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.input_activity)
+
+
+    }
+}
